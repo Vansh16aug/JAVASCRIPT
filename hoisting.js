fd438817ad00e19e4bc0528e1732fef2f50ee1aa
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-02 16:22:38 +0530

    Revised Hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,23 +1,23 @@
-// getName();
+getName();
 
-// // with the start of this program but before the execution of first line all the variables are declared as undefined
-// var x = 7;
+// with the start of this program but before the execution of first line all the variables are declared as undefined
+var x = 7;
 
-// // this func can be accesed all over the program and only in this whole code is assigned 
-// function getName() {
-//   console.log("Namaste Javascript");
-// }
+// this func can be accesed all over the program and only in this whole code is assigned 
+function getName() {
+  console.log("Namaste Javascript");
+}
 
-// // if there is any arrow function then it is considered as a variable and before exceution of the first line undefined is assigned to it
-// var getName2=() =>{
-//     console.log("Namaste javascript");
-// }
+// if there is any arrow function then it is considered as a variable and before exceution of the first line undefined is assigned to it
+var getName2=() =>{
+    console.log("Namaste javascript");
+}
 
-// // another way to declare a function but in this way also this function will act as a variable and undefined will be assigned
-// var getName3 = function(){
-//     console.log("Namaste javascript");
-// }
-// console.log(x);
+// another way to declare a function but in this way also this function will act as a variable and undefined will be assigned
+var getName3 = function(){
+    console.log("Namaste javascript");
+}
+console.log(x);
 
 
 
