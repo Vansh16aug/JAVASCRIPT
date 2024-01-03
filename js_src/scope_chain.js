24085a56616fd9990d4700809d5081d57c40904d
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-03 22:15:57 +0530

    revised scope_chain and lexical enviroment

--- js_src/scope_chain.js
+++ js_src/scope_chain.js
@@ -1,8 +1,15 @@
-function a(){
+function a(){   // global and GEC
     var b=10;
     c();
-    function c(){
+    function c(){   // has access to global and its lexical parent i.e a();
         console.log(b);
     }
 }
 a();
+
+
+// scope -> where we can access thee variable
+
+// lexical enviroment is basically where the piece of code is places 
+
+// LexicalEnviroment -> executionContext of local memory + refrence to lexical enviroment of its parent 
