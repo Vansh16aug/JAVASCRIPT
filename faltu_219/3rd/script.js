78f86764e0082d23847659bb048ce27b3db57ff2
Vansh16aug <kumarvansh16aug@gmail.com>
2024-02-09 11:29:53 +0530

    faltugiri in class

---
+++ faltu_219/3rd/script.js
@@ -0,0 +1,18 @@
+// for(let i=1;i<=5;i++){
+//     let sq=i*i;
+//     console.log(`the square of ${i} is ${sq}`);
+// }
+
+// let str="VanshKumar";
+
+// for(let i=0;i<str.length;i++){
+//     console.log(`${i} is ${str[i]}`);
+// }
+
+function calculateArea(l,b){
+    return l*b;
+}
+let a = prompt(`Enter the first: `);
+let b = prompt(`Enter the second: `);
+
+calculateArea(a,b);
